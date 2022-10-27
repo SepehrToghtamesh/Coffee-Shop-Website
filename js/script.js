a7a9b742c390fdbe5c493560b277585bac5ff57c
Mitra Toghtamesh <mitra.toghtamesh.79@gmail.com>
2022-10-28 00:21:25 +0330

    modifyed the cart-btn's variable name

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@ let navbar = $.querySelector(".navbar");
 $.querySelector("#menu-btn").addEventListener("click", function () {
   navbar.classList.toggle("active");
   searchForm.classList.remove("active");
-  caetItem.classList.remove("active");
+  cartItem.classList.remove("active");
 });
 
 let searchForm = $.querySelector(".search-form");
 $.querySelector("#search-btn").addEventListener("click", function () {
   searchForm.classList.toggle("active");
   navbar.classList.remove("active");
-  caetItem.classList.remove("active");
+  cartItem.classList.remove("active");
 });
 
-let caetItem = $.querySelector(".cart-items-container");
+let cartItem = $.querySelector(".cart-items-container");
 $.querySelector("#cart-btn").addEventListener("click", function () {
-  caetItem.classList.toggle("active");
+  cartItem.classList.toggle("active");
   navbar.classList.remove("active");
   searchForm.classList.remove("active");
 });
@@ -24,5 +24,5 @@ $.querySelector("#cart-btn").addEventListener("click", function () {
 window.onscroll = () => {
   navbar.classList.remove("active");
   searchForm.classList.remove("active");
-  caetItem.classList.remove("active");
+  cartItem.classList.remove("active");
 };
